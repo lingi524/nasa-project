b02135e589c59a80a1250fe1971c71816ba9d81a
lingi524 <linnea.gistrand@gmail.com>
2023-05-25 09:48:10 +0200

    Add Jest testing library and API tests

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,21 @@
+describe('Test GET /launches', () => {
+  test('It should respond with 200 seccess', () => {
+    const response = 200;
+    expect(response).toBe(200);
+  })
+});
+
+describe('Test POST /launches', () => {
+  test('It should respond with 200 seccess', () => {
+
+  });
+  test('It should catch missing required poroperties', () => {
+
+  });
+  test('It should catch invalid dates', () => {
+
+  });
+});
+
+
+
